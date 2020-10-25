f72dcb4bcc7ad61a6c116186571cc3243246c047
rlarjsdn2313 <68491565+rlarjsdn2313@users.noreply.github.com>
2020-10-25 11:34:34 +0900

    d

--- app.js
+++ app.js
@@ -11,27 +11,42 @@ app.get('/', function(req, res) {
     res.render("main.ejs") // render main.ejs
 })
 
-app.get('/drive/:user', function(req, res) {
+app.get("/drive/:user", function(req, res) {
     var id = req.params.user; // get user name
     id = path.parse(id).base; // parse path
 
     console.log(id + " Entered To Drive");
 
     var drive_folder = `./drive/${id}`;
-    console.log(drive_folder)
+
     var file_list = fs.readdirSync(drive_folder); // get file_list
-    console.log(file_list)
 
     var file_list_ = ''; // list that will have real put
     var i = 0; // to count
 
     while (i < file_list.length) {
-        file_list_ = file_list_ + file_list[i] + '<br>';
+        file_list_ = `${file_list_}<a href="/download/${id}/${file_list[i]}">${file_list[i]}</a><br>`;
         i = i + 1;
     }
 
-    res.render("drive.ejs", {user:id, list:file_list_})
+    res.render("drive.ejs", {user:id, list:file_list_});
+});
+
+app.get("/download/:user/:file_name", function(req, res) {
+    var user_name = req.params.user; // get user name
+    user_name = path.parse(user_name).base;
+    var file_name = req.params.file_name;
+    file_name = path.parse(file_name).base;
+
+    var file = `${__dirname}\\drive\\${user_name}\\${file_name}`
+    console.log(user_name, "Download", file_name)
+
+    res.setHeader('Content-Length', file.length);
+    res.write(file,"binary")
+    // res.download(encodeURIComponent(path.basename(`./drive/${user_name}/${file_name}`)));
+    res.redirect(`/drive/${user_name}`)
 })
+
 app.listen(3000); // listen from 3000 port
 
 console.log("Listen From 3000 port")
